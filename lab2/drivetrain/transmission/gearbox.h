5cbfaee78d1535d4b62beebd7937623dd98dcc10
5anji <gitlanm@gmail.com>
2022-09-26 18:07:49 +0300

    add pure virtual class

---
+++ lab2/drivetrain/transmission/gearbox.h
@@ -0,0 +1,9 @@
+class Gear_Ratios;
+
+template <typename T>
+struct Gearbox {
+    virtual void set_coupler(const T&) = 0;
+    virtual void set_ratios(const Gear_Ratios&) = 0;
+    virtual T get_coupler() = 0;
+    virtual Gear_Ratios& get_ratios() = 0;
+};
---
+++ lab2/drivetrain/transmission/manual.h
@@ -0,0 +1,34 @@
+#include "components/gear_ratios.h"
+#include "components/clutch.h"
+
+class Manual {
+    Clutch coupler_type;
+    Gear_Ratios ratios;
+
+public:
+    Manual()
+            : coupler_type(Clutch())
+            , ratios(Gear_Ratios()) {}
+
+    Manual(const Clutch& coupler_type, const Gear_Ratios& ratios)
+            : coupler_type(coupler_type)
+            , ratios(ratios) {}
+
+    void set_coupler(const Clutch& data) {
+        coupler_type.set_torque(const_cast<Clutch&>(data).get_torque());
+        coupler_type.set_friction(const_cast<Clutch&>(data).get_friction());
+    }
+
+    void set_ratios(const Gear_Ratios& v) {
+        ratios.set_g_count(const_cast<Gear_Ratios&>(v).get_g_count());
+        ratios.set_ratios(const_cast<Gear_Ratios&>(v).get_ratios());
+    }
+
+    Clutch& get_coupler() {
+        return coupler_type;
+    }
+
+    Gear_Ratios& get_ratios() {
+        return ratios;
+    }
+};
