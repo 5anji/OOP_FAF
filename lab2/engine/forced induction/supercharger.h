c737581e5b5152682efbf83208c9d02e5d78e4bb
5anji <gitlanm@gmail.com>
2022-09-27 14:03:13 +0300

    add supercharger

---
+++ lab2/engine/forced induction/supercharger.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <cstdint>
+
+class SuperCharger {
+    float ratio;
+public:
+    SuperCharger(const SuperCharger& copy)
+            : ratio(copy.ratio) {}
+
+    float get_pressure_PSI(uint16_t rpm) {
+        return 3 + 0.6 * ratio * (static_cast<float>(rpm) / 1000.f);
+    }
+
+    float get_ratio() { return ratio; }
+};
