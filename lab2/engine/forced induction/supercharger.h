603ffa150a45ae146a419b9eea4d185f68cd64a7
5anji <gitlanm@gmail.com>
2022-09-27 14:17:39 +0300

    add turbocharger

--- lab2/engine/forced induction/supercharger.h
+++ lab2/engine/forced induction/supercharger.h
@@ -3,9 +3,13 @@
 
 class SuperCharger {
     float ratio;
+
 public:
     SuperCharger(const SuperCharger& copy)
             : ratio(copy.ratio) {}
+
+    SuperCharger(float ratio)
+            : ratio(ratio) {}
 
     float get_pressure_PSI(uint16_t rpm) {
         return 3 + 0.6 * ratio * (static_cast<float>(rpm) / 1000.f);
---
+++ lab2/engine/forced induction/turbo.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <cstdint>
+#include <iostream>
+#include <map>
+
+class TurboCharger {
+    std::map<int, float> psi_at_rpm;
+
+public:
+    TurboCharger() {
+        size_t temp;
+        std::cout << "Input max rpm: ";
+        std::cin >> temp;
+        for (size_t i = 500; i <= temp; i += 500) {
+            size_t temp;
+            std::cout << "Input psi for " << i << " rpm: ";
+            std::cin >> temp;
+            psi_at_rpm[i] = temp;
+        }
+    }
+
+    TurboCharger(const TurboCharger& copy)
+            : psi_at_rpm(copy.psi_at_rpm) {}
+
+    TurboCharger(std::map<int, float> psi)
+            : psi_at_rpm(psi) {}
+
+    float get_pressure_PSI(uint16_t rpm) {
+        return psi_at_rpm[rpm];
+    }
+};
