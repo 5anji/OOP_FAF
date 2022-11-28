3f26e5d38cfa19b83b4738e84d7bf1f22965ebab
5anji <gitlanm@gmail.com>
2022-11-28 19:01:00 +0200

    cleanup

--- lab6/main.cpp
+++ lab6/main.cpp
@@ -1,10 +1,5 @@
 #include "controller/controller.h"
 
-#include <iomanip>
-#include <iostream>
-#include <random>
-#include <unistd.h>
-
 int main() {
     Controller controller;
     return controller.run();
